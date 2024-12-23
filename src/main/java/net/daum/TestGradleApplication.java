3666c41c567d8933e3374a349352fc44c9200db8
hasuhyeon913 <hasuzico@gmail.com>
2024-12-23 19:33:16 +0900

    수현 커밋2

--- src/main/java/net/daum/TestGradleApplication.java
+++ src/main/java/net/daum/TestGradleApplication.java
@@ -9,6 +9,7 @@ public class TestGradleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestGradleApplication.class, args);
 		System.out.println("hi~");
+		System.out.println("hi2~");
 	}
 
 }
